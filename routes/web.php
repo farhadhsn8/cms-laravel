131dcdf3d6c5b33b0b55e9d26561a3c5413afc4b
farhadhsn <farhadhsn8@gmail.com>
2021-10-06 17:02:48 +0330

    install breeze

--- routes/web.php
+++ routes/web.php
@@ -17,18 +17,12 @@ Route::get('/', function () {
     return view('landing');
 })->name('landing');
 
-Route::get('/logon', function () {
-    return view('login');
-})->name('login');
-
-Route::get('/register', function () {
-    return view('register');
-})->name('register');
-
-Route::get('/reset-password', function () {
-    return view('reset-password');
-})->name('reset-pass');
-
 Route::get('/post/{id}', function ($id) {
     return view('post');
 })->name('post.show');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
