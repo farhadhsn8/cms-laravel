d0b9473507cab8ba80f48bfd34806d5532f48ad1
farhadhsn <farhadhsn8@gmail.com>
2021-10-16 18:00:46 +0330

    create user

--- app/Http/Controllers/Panel/UserController.php
+++ app/Http/Controllers/Panel/UserController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Panel;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -20,5 +22,22 @@ class UserController extends Controller
     public function edit($id)
     {
         return view('panel.users.edit');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'mobile' => ['required', 'string', 'max:11' ,'min:11' ,'unique:users'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'role' => ['required', 'max:255'],
+        ]);
+
+        $data = $request->only(['name' , 'mobile' , 'email' , 'role']);
+        $data['password'] = Hash::make('password');
+//        dd($data);
+        User::create($data);
+
+        return redirect()->route('users.index');
     }
 }
