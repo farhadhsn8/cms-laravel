9c4ece43d1158cdafb8070f1538180f01189a5f9
farhadhsn <farhadhsn8@gmail.com>
2021-10-19 16:26:35 +0330

    paginate

--- resources/views/layouts/panel.blade.php
+++ resources/views/layouts/panel.blade.php
@@ -4,10 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0;">
     <title>Panel{{$title ?? ''}}</title>
+    {{ $styles ?? '' }}
     <link rel="stylesheet" href="{{asset('blog/panel/css/style.css')}}">
     <link rel="stylesheet" href="{{asset('blog/panel/css/responsive_991.css')}}" media="(max-width:991px)">
     <link rel="stylesheet" href="{{asset('blog/panel/css/responsive_768.css')}}" media="(max-width:768px)">
     <link rel="stylesheet" href="{{asset('blog/panel/css/font.css')}}">
+
 </head>
 <body>
 <div class="sidebar__nav border-top border-left  ">
